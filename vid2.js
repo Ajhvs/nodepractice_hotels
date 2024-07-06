c7ed99a4ba8b7d5e16122158cacb10cb8f242218
Dipanshi <dipanshigupta13@gmail.com>
2024-07-06 14:44:04 +0530

    shift mongodb to online server

--- vid2.js
+++ vid2.js
@@ -13,10 +13,11 @@
 const express = require('express');
 const app = express();
 const db=require('./db');
+require('dotenv').config();
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json()); //req.body
-
+const PORT=process.env.PORT || 3001;
 // const Person= require('./models/Person');
 // const MenuItem=require('./models/Menu');
 
@@ -103,6 +104,6 @@ app.use('/items', menuItemRoutes);
 
 
      
-app.listen(3000, ()=>(
+app.listen(PORT, ()=>(
     console.log('listening on port 3000')
 ))
